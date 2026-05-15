425c96f839fe17a1566df8e183291a5b8ef91d6f
dragonx943 <premieregirl26@gmail.com>
2026-05-16 02:32:05 +0700

    chore: Move all parsers to another package

---
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/kotatsu/madara/vi/TruyenVn.kt
@@ -0,0 +1,15 @@
+package org.koitharu.kotatsu.parsers.site.kotatsu.madara.vi
+
+import org.koitharu.kotatsu.parsers.MangaLoaderContext
+import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.site.kotatsu.madara.MadaraParser
+
+@MangaSourceParser("TRUYENVN", "KhoTruyen", "vi", ContentType.HENTAI)
+internal class TruyenVn(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.TRUYENVN, "truyenvn.shop", 20) {
+	override val listUrl = "truyen-tranh/"
+	override val tagPrefix = "the-loai/"
+	override val datePattern = "dd/MM/yyyy"
+}
